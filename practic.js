fd7eff276c5c5880f83b980d47a1e611660cb35d
Sobid <rustamovsobid95@gmail.com>
2024-03-20 18:04:36 +0500

    typescripct-masala

--- practic.js
+++ practic.js
@@ -1,1 +1,52 @@
 "use strict";
+function reverseArray(array) {
+    const reversedArray = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        reversedArray.push(array[i]);
+    }
+    return reversedArray;
+}
+// Test qilish
+const originalArray = [1, 2, 3, 4, 5];
+const reversedArray = reverseArray(originalArray);
+console.log(reversedArray); // Natija: [5, 4, 3, 2, 1]
+function findMinMax(array) {
+    if (array.length === 0) {
+        throw new Error("Massiv bo'sh.");
+    }
+    let min = array[0];
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+        else if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return [min, max];
+}
+// Test qilish
+const array = [3, 1, 4, 1, 5, 9, 2, 6, 5];
+const [min, max] = findMinMax(array);
+console.log("Eng kichik element:", min);
+console.log("Eng katta element:", max);
+//3-masala
+function separateEvenOdd(array) {
+    const even = [];
+    const odd = [];
+    for (let num of array) {
+        if (num % 2 === 0) {
+            even.push(num);
+        }
+        else {
+            odd.push(num);
+        }
+    }
+    return { even, odd };
+}
+// Test qilish
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const separatedNumbers = separateEvenOdd(arr);
+console.log("Juft sonlar:", separatedNumbers.even);
+console.log("Toq sonlar:", separatedNumbers.odd);
